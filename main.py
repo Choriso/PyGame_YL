c34438eddf9859cd95f475d0c074b12ee0cc092d
kokakolia <razunikolai@gmail.com>
2024-01-03 17:12:53 +0400

    add rules button at start screen

--- main.py
+++ main.py
@@ -27,21 +27,13 @@ class Game:
         self.store.draw_prices(screen)
 
     def update(self):
+        pygame.draw.line(screen, 'black', (360, 0), (360, 600), 5)
+        pygame.draw.line(screen, 'black', (0, 512), (360, 512), 5)
         self.field.draw(screen)
         self.store.update(screen)
         self.hand.update()
 
-    # def take_card_store(self, pos):
-    #     card = self.store.take_cards(pos, all_sprites)
-    #     if card:
-    #         price = self.store.costs(card)
-    #         result = self.goldCoin.buy(price)
-    #         if result:
-    #             return card
-    #     return None
-
     def action(self, pos):
-        print(123)
         res1 = self.store.is_concerning(pos)
         res2 = self.deck.is_concerning(pos)
         if self.hand.can_add():
@@ -59,8 +51,6 @@ class Game:
 running = True
 start_screen()
 game = Game()
-pygame.draw.line(screen, 'black', (357, 0), (357, 600), 5)
-pygame.draw.line(screen, 'black', (0, 515), (357, 515), 5)  # закинуть в рисовалку поля
 
 while running:
     for event in pygame.event.get():
@@ -74,8 +64,6 @@ while running:
     all_sprites.draw(screen)
     all_sprites.update()
     game.update()
-    pygame.draw.line(screen, 'black', (360, 0), (360, 600), 5)
-    pygame.draw.line(screen, 'black', (0, 512), (360, 512), 5)  # закинуть в рисовалку поля
     pygame.display.flip()
 
 pygame.quit()
